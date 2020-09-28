f34a919126fc672659c6f88b3ddb6c962e121b13
unknown <andtryk@gmail.com>
2020-09-28 10:28:24 +0200

    All files in correct places

---
+++ databases/class/db.php
@@ -0,0 +1,36 @@
+<?php
+
+try{
+  $dbUserName = 'root';
+  $dbPassword = ''; // root | admin
+  $dbConnection = 'mysql:host=localhost; dbname=XXXX; charset=utf8mb4'; 
+  // utf8 every character in the world
+  // utf8mb4 every character and also emojies
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // try-catch
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
+  ];
+  $db = new PDO(  $dbConnection, 
+                  $dbUserName, 
+                  $dbPassword , 
+                  $options );
+  
+}catch(PDOException $ex){
+  echo 'error';
+  exit();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
